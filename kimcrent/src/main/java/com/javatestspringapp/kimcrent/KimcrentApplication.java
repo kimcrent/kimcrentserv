d14da05afbf549a13249bda60e14752a05e1b777
Roman Kim <kimcrent@gmail.com>
2024-04-14 00:23:54 +0600

    add proj

---
+++ kimcrent/src/main/java/com/javatestspringapp/kimcrent/KimcrentApplication.java
@@ -0,0 +1,13 @@
+package com.javatestspringapp.kimcrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimcrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimcrentApplication.class, args);
+	}
+
+}
---
+++ kimcrent/src/test/java/com/javatestspringapp/kimcrent/KimcrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatestspringapp.kimcrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimcrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
